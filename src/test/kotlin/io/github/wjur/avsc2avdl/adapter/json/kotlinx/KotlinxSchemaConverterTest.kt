43c24b21c0a6dcd12038efb25efe29c21cc69019
Wojciech Jurczyk <wojtek.jurczyk@gmail.com>
2021-03-10 21:47:19 +0100

    Test reading fields' docs

--- src/test/kotlin/io/github/wjur/avsc2avdl/adapter/json/kotlinx/KotlinxSchemaConverterTest.kt
+++ src/test/kotlin/io/github/wjur/avsc2avdl/adapter/json/kotlinx/KotlinxSchemaConverterTest.kt
@@ -1,6 +1,7 @@
 package io.github.wjur.avsc2avdl.adapter.json.kotlinx
 
 import org.assertj.core.api.Assertions.assertThat
+import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Test
 
 internal class KotlinxSchemaConverterTest {
@@ -27,5 +28,31 @@ internal class KotlinxSchemaConverterTest {
         assertThat(schema.name).isEqualTo("ObjectName")
         assertThat(schema.namespace).isEqualTo("this.is.the.namespace")
         assertThat(schema.documentation).isEqualTo("Super object for test purposes")
+    }
+
+    @Test
+    fun `should read field doc`() {
+        // given
+        @Language("JSON5") val schemaString = """
+            {
+                "type": "record",
+                "name": "ObjectName",
+                "namespace": "this.is.the.namespace",
+                "doc": "Super object for test purposes",
+                "fields": [
+                  {
+                    "name": "someField",
+                    "type": "string",
+                    "doc": "This is someField docs!"
+                  }
+                ]
+            }
+        """.trimIndent()
+
+        // when
+        val schema = converter.convert(schemaString)
+
+        // then
+        assertThat(schema.fields.first().documentation).isEqualTo("This is someField docs!")
     }
 }
